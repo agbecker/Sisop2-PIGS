9d24a5432075205538d181f440e3ebc8300ff48a
Álvaro <108766073+agbecker@users.noreply.github.com>
2025-09-27 09:51:16 -0300

    Adiciona gitignore

--- build/CMakeFiles/client_pigs.dir/src/client/Process.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/client_pigs.dir/src/client/Process.cpp.o: \
- /mnt/f/D/UFRGS/Sisop2/Sisop2-PIGS/src/client/Process.cpp \
- /usr/include/stdc-predef.h \
- /mnt/f/D/UFRGS/Sisop2/Sisop2-PIGS/src/client/Process.h
--- build/CMakeFiles/server_pigs.dir/src/server/Process.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/server_pigs.dir/src/server/Process.cpp.o: \
- /mnt/f/D/UFRGS/Sisop2/Sisop2-PIGS/src/server/Process.cpp \
- /usr/include/stdc-predef.h \
- /mnt/f/D/UFRGS/Sisop2/Sisop2-PIGS/src/server/Process.h
