db00d3b69a9bac9f0dcd7ba6792e5ddf3bba3582
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-10-08 21:21:01 -0300

    Added client interface
    Missing command result printing because it requires the result of the operation

--- src/client/Interface.cpp
+++ src/client/Interface.cpp
@@ -1,2 +1,66 @@
 #include "Interface.h"
 using namespace std;
+
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <arpa/inet.h>
+#include <regex>
+
+bool ipv4IsValid(const std::string& ipAddress) {
+    const std::string reg = "(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])";
+    
+    const std::regex ipv4Regex("^" + reg + "\\." + reg + "\\." + reg + "\\." + reg + "$");
+
+    return std::regex_match(ipAddress, ipv4Regex);
+}
+
+inline std::string current_time_format() {
+    auto now = std::chrono::system_clock::now();
+    auto time_type = std::chrono::system_clock::to_time_t(now);
+    auto *local_time = std::localtime(&time_type);
+
+    std::stringstream string_stream;
+    string_stream << std::put_time(local_time, "%Y-%m-%d %H:%M:%S");
+
+    return string_stream.str();
+}
+
+Command Interface::getCommand() {
+    Command cmd;
+    std::string addr;
+
+    std::cin >> addr >> cmd.amount;
+
+    if(!ipv4IsValid(addr)) {
+        std::cerr << current_time_format() << " | Invalid IP address format. Try again." << std::endl;
+        return getCommand();
+    }
+    if(cmd.amount <= 0) {
+        std::cerr << current_time_format() << " | Amount must be positive. Try again." << std::endl;
+        return getCommand();
+    }
+
+    // convert string to in_addr (ip struct)
+    inet_pton(AF_INET, addr.c_str(), &(cmd.dest));
+
+    return cmd;
+}
+
+void Interface::printCommandResult(double new_balance, int command_count) {
+    std::string server_ip = inet_ntoa(this->server_addr);
+    std::string dest_ip = inet_ntoa(this->current_command.dest);
+
+    std::cout << "\n" << current_time_format() << " server " << server_ip << " id_req " << command_count;
+    std::cout << " dest " << dest_ip << " value " << this->current_command.amount << " new_balance " << new_balance << std::endl;
+}
+
+void Interface::run() {
+    while (true) {
+        current_command = getCommand();
+        // TODO:
+        // new_balance, id_req = executeCommand(current_command);
+        // this->printCommandResult(new_balance, id_req);
+    }
+}
--- src/client/Interface.h
+++ src/client/Interface.h
@@ -1,6 +1,27 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include <string>
+#include <netinet/in.h>
+
+typedef struct Command{
+    struct in_addr dest; // destination IP
+    double amount;       // amount to transfer
+} Command;
+
+class Interface {
+    private:
+        Command current_command;
+        struct in_addr server_addr; 
+        
+        Command getCommand();
+        void printCommandResult(double new_balance, int command_count);
+
+    public: 
+        Interface(in_addr server_addr) : server_addr(server_addr) { }
+        void run();
+    };
+
 
 
 #endif
