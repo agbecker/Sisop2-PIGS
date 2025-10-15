c7c62e1782fd575e15e08944401373319e2ed590
Álvaro <108766073+agbecker@users.noreply.github.com>
2025-10-15 19:12:02 -0300

    Passa clientdata para o process

--- src/server/Process.h
+++ src/server/Process.h
@@ -1,11 +1,26 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <queue>
+#include <mutex>
 
+// Bibliotecas para conexão
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+struct ClientData {
+    std::string ip; // IP do cliente, usado como chave
+    int balance; // Saldo do cliente
+    int seq_num; // Último número de sequência registrado do cliente
 
-
-
-
+    ClientData(std::string i, int b, int s): ip(i), balance(b), seq_num(s) {}
+};
 
 #endif
