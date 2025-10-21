a447f8d8577e7782809ca8a686a784bd2394293b
Álvaro <108766073+agbecker@users.noreply.github.com>
2025-10-21 09:19:39 -0300

    Descoberta com timeout

--- src/client/Discovery.cpp
+++ src/client/Discovery.cpp
@@ -6,7 +6,7 @@ bool discoverServer(struct sockaddr_in &serv_addr) {
     int sockfd; // ID do socket
     socklen_t servlen = sizeof(struct sockaddr_in); // Tamanho do IP do servidor
     struct hostent *server; // Servidor
-    char buf[BUFFER_SIZE] = "WHERE IS SERVER OINK"; // Mensagem
+    char buf[BUFFER_SIZE]; // Mensagem
 
     // Cria socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -23,18 +23,33 @@ bool discoverServer(struct sockaddr_in &serv_addr) {
         return false;
     }
 
+    // Configura timeout para recvfrom
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = TIMEOUT;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        perror("setsockopt timeout");
+        close(sockfd);
+        return false;
+    }
+
     // Inicializa socket (IPv4, porta BROADCAST_PORT, envia para o IP de broadcast)
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(BROADCAST_PORT);
 	serv_addr.sin_addr.s_addr = inet_addr("255.255.255.255");
     bzero(&(serv_addr.sin_zero), 8);
 
-    // Envia a mensagem
-    int n = sendto(sockfd, buf, strlen(buf), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
+    int n = -1;
+    while(n < 0) {
+        // Envia a mensagem
+        strcpy(buf, "WHERE IS SERVER OINK");
+        n = sendto(sockfd, buf, strlen(buf), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
 
-    // Aguarda o ACK
-    bzero(buf, BUFFER_SIZE);
-    n = recvfrom(sockfd, buf, BUFFER_SIZE, 0, (struct sockaddr *) &serv_addr, &servlen);
+        // Aguarda o ACK
+        bzero(buf, BUFFER_SIZE);
+        n = recvfrom(sockfd, buf, BUFFER_SIZE, 0, (struct sockaddr *) &serv_addr, &servlen);
+        cout << "enviando broadcast" << endl;
+    }
 
     // Debug
     string server_ip = inet_ntoa(serv_addr.sin_addr);
