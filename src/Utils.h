3b86fb36d5c7057dd72d684294847a1535aa4c08
Álvaro <108766073+agbecker@users.noreply.github.com>
2025-10-26 16:28:47 -0300

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -13,7 +13,6 @@ typedef struct RequestReply
 // Status possíveis:
 #define RR_INVALID 1    // O dado não tem significado, não deve ser processado
 #define RR_SEND 2       // O dado é válido e deve ser enviado ao servidor
-#define RR_WAITING 3    // Aguardando resposta do servidor
 #define RR_OK 4         // O dado foi processado corretamente
 #define RR_BALANCE 5    // Erro no processamento por saldo insuficiente
 #define RR_NUMBER 6     // Erro no processamento por número de sequência errado
